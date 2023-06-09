b48651a0998826a805484c2b58f4790cf58ebfdb
Inderpreet <inder17375@gmail.com>
2023-06-09 12:06:43 +0530

    RabbitMQ-DirectExchange : Direct Exchange Completed

---
+++ rabbitmq/src/main/java/com/microservice/rabbitmq/model/Response.java
@@ -0,0 +1,12 @@
+package com.microservice.rabbitmq.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Response {
+    private String response ;
+}
